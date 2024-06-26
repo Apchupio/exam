3b61cf8c953b00048259e0a10a94848103bf775a
Apchupio <146679109+Apchupio@users.noreply.github.com>
2024-06-26 18:32:41 +1000

    Update exam.py
    добавление комментария к коду

--- exam.py
+++ exam.py
@@ -1,9 +1,11 @@
 def print_string_n_times(string, n):
+# Добавим условный оператор для выполнения следующей строки    
     if n > 0:
         print(string)
         print_string_n_times(string, n-1)
-
+# Добавим метод для вывода строки с клавиатуры
 user_string = input("Введите строку: ")
+# Добавим метод для для количества строк которые нужно вывести
 user_n = int(input("Введите количество повторений: "))
 
 print_string_n_times(user_string, user_n)
